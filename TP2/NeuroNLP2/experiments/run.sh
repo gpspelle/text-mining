415d369a971f0a709462217c50e2c2b819ca6050
Gabriel Pellegrino <gabriel.pellegrino@student-cs.fr>
2021-02-11 23:31:58 +0100

    fix(q1): add q1

--- TP2/NeuroNLP2/experiments/run.sh
+++ TP2/NeuroNLP2/experiments/run.sh
@@ -2,5 +2,5 @@
 _quaero="TP_ISD2020/QUAERO_FrenchPress"
 python ner.py --config "quaero-100-demi.json" --num_epochs 10 --batch_size 16 \
  --loss_type sentence --optim sgd --learning_rate 0.01 --lr_decay 0.99999 --grad_clip 0.0 --warmup_steps 10 --weight_decay 0.0 --unk_replace 0.0 \
- --embedding sskip --embedding_dict "QUAERO_FrenchPress-w2v.vec.gz" --model_path "models/cbow_w2v_FrenchMed.model" \
+ --embedding sskip --embedding_dict "QUAERO_FrenchPress-w2v.vec.gz" --model_path "models/fasttext_FrenchPress.model" \
  --train "$_quaero/fra4_ID.train" --dev "$_quaero/fra4_ID.dev" --test "$_quaero/fra4_ID.test"
